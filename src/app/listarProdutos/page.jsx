1aeba8e40a6124c013758182e89c035df790ac6a
Kaique Barbosa <kaiquebarbosa2103@gmail.com>
2024-11-04 22:04:18 -0300

    incrementando o axios no front

--- src/app/listarProdutos/page.jsx
+++ src/app/listarProdutos/page.jsx
@@ -1,20 +1,45 @@
 "use client"
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import axiosInstance from "@/utils/axiosInstance";
+import { useEffect, useState } from "react";
 
 export default function ListarProdutos() {
-
   const router = useRouter();
+  const [data, setData] = useState([{}])
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const resposta = await axiosInstance.get('/produtos')
+          setData(resposta.data)
+        } catch (error) {
+          if (error.response) {
+            // A resposta do servidor foi recebida, mas contém erro (status code diferente de 2xx)
+            console.error('Erro de resposta:', error.response.data);
+          } else if (error.request) {
+            // A requisição foi feita, mas nenhuma resposta foi recebida
+            console.error('Erro de requisição:', error.request);
+          } else {
+            // Outro tipo de erro ocorreu
+            console.error('Erro ao configurar requisição:', error.message)
+        }
+      }
+      }
+      fetchData()
+    }, [])
+    
+  
 
   return (
-    <div className="flex w-full items-center justify-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex w-full md:w-[80%] m-auto items-center justify-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 w-full justify-center items-center m-auto">
         <div className=" flex justify-around w-full">
           <h1 className="text-4xl font-bold">Lista de Produtos</h1>
           {/* <Link href={'/'} className="text-4xl font-bold">Cadastrar Produto</Link> */}
           <button onClick={() => {router.push('/cadastroProdutos')}} className="btn btn-active btn-primary">Cadastrar Produto</button>
         </div>
-        <div className="overflow-x-auto w-[60rem] shadow-lg shadow-indigo-500/50 p-2 rounded-lg">
+        <div className="overflow-x-auto w-[40rem] xl:w-[60rem] shadow-lg shadow-indigo-500/50 p-2 rounded-lg">
           <table className="table table-zebra">
             {/* head */}
             <thead>
@@ -26,12 +51,13 @@ export default function ListarProdutos() {
               </tr>
             </thead>
             <tbody>
-              {/* row 1 */}
+              {/* row  */}
+            {data.map((data)=>{
               <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <th>{data.id}</th>
+                <td>{data.nome}</td>
+                <td>{data.quantidade}</td>
+                <td>{data.preco}</td>
                 <td>
                   <div className="flex items-center justify-center gap-5 my-2">
                     <button className="btn btn-info">Editar</button>
@@ -39,33 +65,8 @@ export default function ListarProdutos() {
                   </div>
                 </td>
               </tr>
-              {/* row 2 */}
-              <tr>
-                <th>2</th>
-                <td>Hart Hagerty</td>
-                <td>Desktop Support Technician</td>
-                <td>Purple</td>
-                <td>
-                  <div className="flex items-center justify-center gap-5 my-2">
-                    <button className="btn btn-info">Editar</button>
-                    <button className="btn btn-error">Excluir</button>
-                  </div>
-                </td>
-              </tr>
-              {/* row 3 */}
-              <tr>
-                <th>3</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red</td>
-                <td>
-                  <div className="flex items-center justify-center gap-5 my-2">
-                    <button className="btn btn-info">Editar</button>
-                    <button className="btn btn-error">Excluir</button>
-                  </div>
-                </td>
-              </tr>
-              {/* row 4 */}
+            })}
+
             </tbody>
           </table>
           <div className="flex justify-around mt-10">
